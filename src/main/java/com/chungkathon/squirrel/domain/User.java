91e2cc9e7118ea1bd68477ecedc2d80eacee3176
cherrynniii <chaelin20760@gmail.com>
2024-11-07 20:17:15 +0900

    feat: User 엔티티 생성 및 권한 설정을 위한 Role enum 생성

---
+++ src/main/java/com/chungkathon/squirrel/domain/User.java
@@ -0,0 +1,52 @@
+package com.chungkathon.squirrel.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+    @Id
+    @GeneratedValue
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    /* 각 사용자의 권한을 관리할 Enum 클래스 (ROLE_ADMIN, ROLE_USER 등)
+    * Spring Security에는 권한 코드에 항상 ROLE_이 앞에 있어야 함 */
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+}
