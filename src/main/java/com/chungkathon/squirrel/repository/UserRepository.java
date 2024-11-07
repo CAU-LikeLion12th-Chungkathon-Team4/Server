e965da329d59974c0b68e13d9ed69f517dd82ad5
cherrynniii <chaelin20760@gmail.com>
2024-11-07 20:32:49 +0900

    feat: UserRepository 추가

--- src/main/java/com/chungkathon/squirrel/repository/UserRepository.java
+++ src/main/java/com/chungkathon/squirrel/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.chungkathon.squirrel.repository;
 
+import com.chungkathon.squirrel.domain.User;
+import java.util.Optional;
+
 public interface UserRepository {
+    // 중복 가입 확인을 위함
+    Optional<User> findByEmail(String email);
 }
