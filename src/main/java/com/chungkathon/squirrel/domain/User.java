c1961dbef4c0400c78a92a5026cb725f43932832
cherrynniii <chaelin20760@gmail.com>
2024-11-09 02:47:51 +0900

    feat: h2로 전환 후 작동(실행 시 에러 X) + Users 테이블 생성 확인

--- src/main/java/com/chungkathon/squirrel/domain/User.java
+++ src/main/java/com/chungkathon/squirrel/domain/User.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import org.antlr.v4.runtime.misc.NotNull;
 
 @Entity
+@Table(name = "Users")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
