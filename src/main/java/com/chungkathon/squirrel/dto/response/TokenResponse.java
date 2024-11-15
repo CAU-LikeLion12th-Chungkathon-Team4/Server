f7b78ef82f2d76b198057c2a0656957f3d1bee03
cherrynniii <chaelin20760@gmail.com>
2024-11-16 08:20:21 +0900

    fix: 회원가입/로그인 시 id도 같이 반환

--- src/main/java/com/chungkathon/squirrel/dto/response/TokenResponse.java
+++ src/main/java/com/chungkathon/squirrel/dto/response/TokenResponse.java
@@ -2,9 +2,10 @@ package com.chungkathon.squirrel.dto.response;
 
 public record TokenResponse(
         String accessToken,
-        String refreshToken
+        String refreshToken,
+        Long id
 ) {
-    public static TokenResponse of(String accessToken, String refreshToken) {
-        return new TokenResponse(accessToken, refreshToken);
+    public static TokenResponse of(String accessToken, String refreshToken, Long id) {
+        return new TokenResponse(accessToken, refreshToken, id);
     }
 }
