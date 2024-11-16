9689b5ed80c49cbdd84fac0e3094a71dfb138ddb
cherrynniii <chaelin20760@gmail.com>
2024-11-16 09:34:03 +0900

    feat: id 중복 테스트 api 추가

--- src/main/java/com/chungkathon/squirrel/controller/JoinController.java
+++ src/main/java/com/chungkathon/squirrel/controller/JoinController.java
@@ -3,14 +3,20 @@ package com.chungkathon.squirrel.controller;
 import com.chungkathon.squirrel.domain.Member;
 import com.chungkathon.squirrel.dto.request.JoinRequest;
 import com.chungkathon.squirrel.dto.request.LoginRequest;
+import com.chungkathon.squirrel.dto.request.RedundancyCheckRequest;
 import com.chungkathon.squirrel.dto.response.TokenResponse;
 import com.chungkathon.squirrel.jwt.JwtTokenProvider;
 import com.chungkathon.squirrel.repository.MemberJpaRepository;
 import com.chungkathon.squirrel.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -30,6 +36,14 @@ public class JoinController {
 
         // 토큰 반환
         return TokenResponse.of(accessToken, refreshToken, member.getId());
+    }
+
+    @PostMapping("/join/check")
+    public ResponseEntity<Map<String, Boolean>> checkUsername(@RequestBody RedundancyCheckRequest redundancyCheckRequest) {
+        boolean isExist = memberService.joinCheck(redundancyCheckRequest);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("isExist", isExist);
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/login")
